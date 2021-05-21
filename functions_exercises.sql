3b2938d5164b67b4a8b4eae2d9e764c8f5739370
Karla Jara <jara.karla.43@gmail.com>
2021-05-21 14:14:57 -0500

    added code for functions exercise

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -12,17 +12,26 @@ SELECT * FROM employees WHERE last_name LIKE '%q%';
 # Find all employees with first names 'Irena', 'Vidya', or 'Maya' — 709 rows (Hint: Use IN)
 SELECT * FROM employees WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya';
 
-# Find all employees whose last name starts with 'E' — 7,330 rows
-SELECT * FROM employees WHERE last_name LIKE 'E';
-
 # Add a condition to the previous query to find everybody with those names who is also male — 441 rows.
 SELECT * FROM employees WHERE gender = 'M' AND (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya');
 
 #Find all employees whose last name starts or ends with 'E' — 30,723 rows.
-SELECT * FROM employees WHERE last_name LIKE 'e%' OR last_name LIKE '%e';
+SELECT CONCAT(first_name, ' ', last_name, '!') FROM employees WHERE last_name LIKE 'e%' OR last_name LIKE '%e';
 
 # Duplicate the previous query and update it to find all employees whose last name starts and ends with 'E' — 899 rows.
 SELECT * FROM employees WHERE last_name LIKE 'e%' AND last_name LIKE '%e';
 
 
 # SELECT first_name, last_name FROM employees ORDER BY last_name DESC, first_name LIMIT 200;
+
+# Find all employees born on Christmas — 842 rows.
+SELECT * FROM employees WHERE month(birth_date) = 12 AND day(birth_date) = 25;
+
+# Find all employees hired in the 90s and born on Christmas — 362 rows.
+SELECT * FROM employees WHERE year(hire_date) BETWEEN 1990 AND 1999 AND month(birth_date) = 12 AND day(birth_date) = 25;
+
+# Change the query for employees hired in the 90s and born on Christmas such that the first result is the oldest employee who was hired last. It should be Khun Bernini.
+SELECT * FROM employees WHERE year(hire_date) BETWEEN 1990 AND 1999 AND month(birth_date) = 12 AND day(birth_date) = 25 ORDER BY birth_date ASC, hire_date DESC;
+
+#For your query of employees born on Christmas and hired in the 90s, use datediff() to find how many days they have been working at the company (Hint: You might also need to use now() or curdate()).
+SELECT CONCAT (datediff(hire_date - curdate())) FROM employees WHERE year(hire_date) BETWEEN 1990 AND 1999 AND month(birth_date) = 12 AND day(birth_date) = 25;
