34ba21c73d16f432e20d1a3273a6484852913db4
Karla Jara <jara.karla.43@gmail.com>
2021-05-23 20:32:37 -0500

    refactored limit exercise code

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -2,12 +2,21 @@ USE employee;
 
 # SQL Limit Exercise
 
-# 2 List the first 10 distinct last name sorted in descending order.
+# 2 List the first 10 distinct last name sorted in descending order- starts: Zykh, ends: Zongker
 SELECT DISTINCT last_name FROM employees WHERE last_name LIKE 'Z%' ORDER BY last_name DESC LIMIT 10;
 
 # 3 Create a query to get the top 5 salaries and display just the employees number from the salaries table. The employee number results should be:
-SELECT DISTINCT salary FROM salaries ORDER BY salary DESC LIMIT 5;
+# 43624
+# 43624
+# 254466
+# 47978
+# 253939
+SELECT emp_no FROM salaries ORDER BY salary DESC LIMIT 5;
 
 # 4 Try to think of your results as batches, sets, or pages. The first five results are your first page. The five after that would be your second page, etc. Update the previous query to find the tenth page of results. The employee number results should be:
+# 254466
+# 492164
+# 66793
+# 492164
 #HINT: LIMIT and OFFSET can be used to create multiple pages of data. What is the relationship between OFFSET (number of results to skip), LIMIT (number of results per page), and the page number?
-SELECT DISTINCT emp_no FROM salaries ORDER BY salary DESC LIMIT 5 OFFSET 50;
+SELECT emp_no FROM salaries ORDER BY salary DESC LIMIT 5 OFFSET 46;
--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -15,26 +15,10 @@ SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY 
 SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY last_name, first_name;
 
 
-# Find all employees whose last name starts with 'E' — 7,330 rows order by employee #
+# 5 Update your queries for employees with 'E' in their last name to sort the results by their employee number. Your results should not change!
+# results as described above
 SELECT * FROM employees WHERE last_name LIKE 'e%' ORDER BY emp_no;
 
-# Find all employees with a 'q' in their last name — 1,873 rows.
-SELECT * FROM employees WHERE last_name LIKE '%q%';
-
-# Find all employees with first names 'Irena', 'Vidya', or 'Maya' — 709 rows (Hint: Use IN)
-SELECT * FROM employees WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya';
-
-# Find all employees whose last name starts with 'E' — 7,330 rows
-SELECT * FROM employees WHERE last_name LIKE 'E';
-
-# Add a condition to the previous query to find everybody with those names who is also male — 441 rows.
-SELECT * FROM employees WHERE gender = 'M' AND (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya');
-
-#Find all employees whose last name starts or ends with 'E' — 30,723 rows.
-SELECT * FROM employees WHERE last_name LIKE 'e%' OR last_name LIKE '%e';
-
-# Duplicate the previous query and update it to find all employees whose last name starts and ends with 'E' — 899 rows.
-SELECT * FROM employees WHERE last_name LIKE 'e%' AND last_name LIKE '%e';
-
-
-# SELECT first_name, last_name FROM employees ORDER BY last_name DESC, first_name LIMIT 200;
+# 6 Now reverse the sort order for both queries and compare results.
+# Not sure about the results being asked for
+SELECT * FROM employees WHERE last_name LIKE '%e' ORDER BY emp_no;
